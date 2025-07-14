ee7f70331005824c380ec75b9138cc64df009f07
Deepak Singh <deepaky954000@gmail.com>
2025-07-14 09:43:59 +0530

    updated db

--- src/configs/database.config.js
+++ src/configs/database.config.js
@@ -3,9 +3,15 @@ import { Sequelize } from "sequelize";
 
 const sequelize = new Sequelize(env.DB_NAME, env.DB_USER, env.DB_PASS, {
   host: env.DB_HOST,
+  port: env.DB_PORT,
   dialect: env.DB_DIALECT,
-  port:env.DB_PORT,
   logging: false,
+  pool: {
+    max: 20, // increase this based on your DB's max_connections
+    min: 2,
+    acquire: 30000, // how long Sequelize will try to get a connection (ms)
+    idle: 10000, // how long a connection can stay idle before being released
+  },
 });
 
 export default sequelize;
